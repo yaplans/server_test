439779e510f61a55cda209afb44d1e53f6b63be9
yaplans <yaplans@mail.ru>
2019-02-20 18:21:12 +0600

    Подправил

--- html/main1.php
+++ html/main1.php
@@ -63,31 +63,6 @@ function l_fcreate($p){
 	}
 }
 
-/**
- * Получаем id - четыре последние цифры времени
- * в секундах
- * */
-function l_make_id(){
-	$m_time=time();
-	$id=$m_time%10000;//остаток
-	return substr("0000".$id,-4);
-}
-/**
- * Ищем самый старый файл по маске ????.gid
- * */
-function l_find_f_id1($p){
-	$m_file=glob($p);
-	$m_t=0;
-	$m_f="";
-	foreach ($m_file as $key => $value){
-		if($m_t<filemtime($value)){
-			$m_f=$value;// самый старый
-			$m_t=filemtime($value);
-		}
-	}
-	return $m_f;
-}
-
 //======================================//
 // Start!
 session_start();
@@ -95,11 +70,11 @@ echo "<br>";
 var_dump($_SESSION);
 //~ unset($_SESSION["m_first"]);
 //~ unset($_SESSION["m_second"]);
-//~ exit("Очистили");
+exit("Проверим сессию!");
 
 //---------------------------------------//
 // vars
-//~ $m_wate_file="2wate.txt";
+$m_wate_file="2wate.txt";
 $m_timeold_file=10;// 10минут
 //---------------------------------------//
 
@@ -109,61 +84,32 @@ echo "	//нет ты не ПЕРВЫЙ - проверим дальше"."<br>";
 	//~ Ты ВТОРОЙ - ДА - иди на 222
 	if(!isset($_SESSION["m_second"])){
 echo "	//нет ты не ВТОРОЙ - проверим дальше"."<br>";
-		$m_wate_file=l_find_f_id1("????.gid");
-var_dump($m_wate_file);
-//~ exit;
-
 		if(file_exists($m_wate_file)){
 echo "	//~ ??? файл 2wate.txt есть, но старше 10мин???"."<br>";
 			$m_mtime=filemtime($m_wate_file);
 			$m_time=time();
 			if(($m_time-$m_mtime)/60>$m_timeold_file){
-				//~ unset($m_wate_file);
-				unlink($m_wate_file);
+				unset($m_wate_file);
 				echo "	//~ ??? файл 2wate.txt - удаляем."."<br>";
 			}
 		}
-		
-		
-		
-		
 		//~ ??? файл 2wate.txt есть
 		if(file_exists($m_wate_file)){
 echo "	//~  файл 2wate.txt есть"."<br>";
-			$id_2=l_make_id();
-			$id_1=basename($m_wate_file,".gid");
-			$m_wate_file = $id_1."_".$id_2.".gid";
-			echo $m_wate_file;
-			echo "<br>";
 		//~ ты - ВТОРОЙ
 			$_SESSION["m_second"] = TRUE;
-			$_SESSION["id"] = $id_2;
-			$_SESSION["file"] = $m_wate_file;
 		//~ иди на 222
 		} else {
 		//~ ты - ПЕРВЫЙ
-			$id_1=l_make_id();
-			echo $id_1;
-			echo "<br>";
-			$m_wate_file = $id_1.".gid";
-			
 			$_SESSION["m_first"] = TRUE;
-			$_SESSION["id"] = $id_1;
 echo "	//~ создать файл 2wate.txt=".$m_wate_file."<br>";
 			l_fcreate($m_wate_file);
-			$_SESSION["file"] = $m_wate_file;
 		//~ иди на 222
 		}
 	}
-} else {
-	//~ $m_wate_file=l_find_f_id1($_SESSION["id"]."*.gid");
-	$_SESSION["file"] = l_find_f_id1($_SESSION["id"]."*.gid");
 }
 echo "	//~ 222"."<br>";
 
-$m_wate_file=$_SESSION["file"];
-
-//~ exit;
 //~ )))222
 
 $num_gamer="first";
--- html/main_20190220.php
+++ html/main_20190220.php
@@ -63,31 +63,6 @@ function l_fcreate($p){
 	}
 }
 
-/**
- * Получаем id - четыре последние цифры времени
- * в секундах
- * */
-function l_make_id(){
-	$m_time=time();
-	$id=$m_time%10000;//остаток
-	return substr("0000".$id,-4);
-}
-/**
- * Ищем самый старый файл по маске ????.gid
- * */
-function l_find_f_id1($p){
-	$m_file=glob($p);
-	$m_t=0;
-	$m_f="";
-	foreach ($m_file as $key => $value){
-		if($m_t<filemtime($value)){
-			$m_f=$value;// самый старый
-			$m_t=filemtime($value);
-		}
-	}
-	return $m_f;
-}
-
 //======================================//
 // Start!
 session_start();
@@ -99,7 +74,7 @@ var_dump($_SESSION);
 
 //---------------------------------------//
 // vars
-//~ $m_wate_file="2wate.txt";
+$m_wate_file="2wate.txt";
 $m_timeold_file=10;// 10минут
 //---------------------------------------//
 
@@ -109,10 +84,6 @@ echo "	//нет ты не ПЕРВЫЙ - проверим дальше"."<br>";
 	//~ Ты ВТОРОЙ - ДА - иди на 222
 	if(!isset($_SESSION["m_second"])){
 echo "	//нет ты не ВТОРОЙ - проверим дальше"."<br>";
-		$m_wate_file=l_find_f_id1("????.gid");
-var_dump($m_wate_file);
-//~ exit;
-
 		if(file_exists($m_wate_file)){
 echo "	//~ ??? файл 2wate.txt есть, но старше 10мин???"."<br>";
 			$m_mtime=filemtime($m_wate_file);
@@ -123,47 +94,23 @@ echo "	//~ ??? файл 2wate.txt есть, но старше 10мин???"."<br>
 				echo "	//~ ??? файл 2wate.txt - удаляем."."<br>";
 			}
 		}
-		
-		
-		
-		
 		//~ ??? файл 2wate.txt есть
 		if(file_exists($m_wate_file)){
 echo "	//~  файл 2wate.txt есть"."<br>";
-			$id_2=l_make_id();
-			$id_1=basename($m_wate_file,".gid");
-			$m_wate_file = $id_1."_".$id_2.".gid";
-			echo $m_wate_file;
-			echo "<br>";
 		//~ ты - ВТОРОЙ
 			$_SESSION["m_second"] = TRUE;
-			$_SESSION["id"] = $id_2;
-			$_SESSION["file"] = $m_wate_file;
 		//~ иди на 222
 		} else {
 		//~ ты - ПЕРВЫЙ
-			$id_1=l_make_id();
-			echo $id_1;
-			echo "<br>";
-			$m_wate_file = $id_1.".gid";
-			
 			$_SESSION["m_first"] = TRUE;
-			$_SESSION["id"] = $id_1;
 echo "	//~ создать файл 2wate.txt=".$m_wate_file."<br>";
 			l_fcreate($m_wate_file);
-			$_SESSION["file"] = $m_wate_file;
 		//~ иди на 222
 		}
 	}
-} else {
-	//~ $m_wate_file=l_find_f_id1($_SESSION["id"]."*.gid");
-	$_SESSION["file"] = l_find_f_id1($_SESSION["id"]."*.gid");
 }
 echo "	//~ 222"."<br>";
 
-$m_wate_file=$_SESSION["file"];
-
-//~ exit;
 //~ )))222
 
 $num_gamer="first";
